281dfdb9bd8fb44130a5be998ec4d0f8c6f7d127
RamandeepKaur-alt <141054407+RamandeepKaur-alt@users.noreply.github.com>
2025-12-09 21:11:50 +0530

    All Fixed

--- server.js
+++ server.js
@@ -56,8 +56,7 @@ app.get("/api/test2", (req , res)=>{
   });
 });
 
-console.log("CI/CD working");
-
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
